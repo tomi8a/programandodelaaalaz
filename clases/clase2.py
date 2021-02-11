8e9fa9f501e532a2f25c322b08df25e784ea509f
tomi8a <ochoa.tomas@uces.edu.co>
2021-02-11 07:43:47 -0500

    subiendo

---
+++ clases/clase2.py
@@ -0,0 +1,7 @@
+pruebav=True
+pruebaf=False
+print (pruebav)
+print (pruebaf)
+edad=27
+isMayorEdad = edad >= 18
+print(isMayorEdad)
---
+++ clases/operaciones.py
@@ -0,0 +1,9 @@
+numeroA= 87
+numeroB= 83
+sumar = numeroA + numeroB
+print  ("el resultado de la suma es",sumar)
+restar = numeroA - numeroB
+multiplicar = numeroA * numeroB
+print (multiplicar)
+exponente = numeroA**numeroB
+print (exponente)
